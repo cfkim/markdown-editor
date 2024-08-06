b2bc9960665e0b8dd36edc395e3e9b0c35ffe756
cfkim <christine.fth.kim@gmail.com>
2024-08-06 01:05:12 -0400

    fix box formatting.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [value, setValue] = useState("");
+  function updatePreview(e) {
+    setValue(e.target.value);
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="main__content">
+          <div className="editor__container">
+            <h2>Write</h2>
+            <div className="editing__box">
+              <textarea name="input" id="input" onChange={updatePreview} value={value}></textarea>
+            </div>
+          </div>
+          
+          <div className="preview__content">
+            <h2>Preview</h2>
+            <div className="preview__text">
+              <ReactMarkdown id="preview">{value}</ReactMarkdown>
+              <p></p>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
